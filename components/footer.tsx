e8a78df78357b55e7f37cbf56ce4d6fcb761d1cb
IGN-Styly <claudioresendeptbr@gmail.com>
2025-05-29 19:57:45 +0100

    footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <div className="mt-6 pt-6 pb-6 border-t border-gray-200 text-center text-sm text-gray-600">
-      © 2024 Your Brand. All rights reserved.
+      Site desenvolvido por Cláudio Torres | Curso Técnico de Gestão e
+      Programação de Sistemas Informáticos | Ciclo formativo 2024/2027 <br />
+      Sítio atualizado em 2025/05/29 <br />
+      Tecnologia NextJS
     </div>
   );
 }
