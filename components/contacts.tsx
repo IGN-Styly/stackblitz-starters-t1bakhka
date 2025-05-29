afc91ed5050ac16bb14e738e40599941a92243dd
IGN-Styly <claudioresendeptbr@gmail.com>
2025-05-29 19:53:45 +0100

    fix

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Contacts() {
   return (
@@ -13,7 +13,7 @@ export default function Contacts() {
             </h1>
 
             <p className="text-lg leading-relaxed text-muted-foreground max-w-lg">
-              Localidade: Braga, Portugal, Escola Secundária Sá de Miranda{' '}
+              Localidade: Braga, Portugal, Escola Secundária Sá de Miranda{" "}
               <br />
               Telefone: 253 200 980
             </p>
@@ -23,7 +23,6 @@ export default function Contacts() {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.5742383527413!2d-8.420827923450641!3d41.55681498546814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24feeb82a77e57%3A0x6be9135bed346e47!2sEscola%20Secund%C3%A1ria%20S%C3%A1%20de%20Miranda!5e0!3m2!1sen!2spt!4v1748533713416!5m2!1sen!2spt"
               width="600"
               height="450"
-              allowfullscreen=""
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
